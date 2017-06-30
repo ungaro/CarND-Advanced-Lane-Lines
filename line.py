e3252d16dc2ffdf53f646ee8225976a06b66ef12
Alp Guneysel <alp.guneysel@gmail.com>
2017-06-30 14:25:08 +0300

    Curvature function fix

--- line.py
+++ line.py
@@ -15,12 +15,28 @@ class Line:
         self.midx = 640
         self.ym_per_pix = 30/720 # meters per pixel in y dimension
         self.xm_per_pix = 3.7/880 # meters per pixel in x dimension
-        self.curvature = 0
-       
+        self.curvature = None
+        
+        self.n = 15
+        # x values of the last n fits of the line
+        self.recent_xfitted = [] 
+        #average x values of the fitted line over the last n iterations
+        self.bestx = None     
+        #polynomial coefficients averaged over the last n iterations
+        self.best_fit = None  
+        #polynomial coefficients for the most recent fit
+        self.current_fit = [np.array([False])]  
+ 
+
+
+
+
+
        
     def update_fits(self, left_fit, right_fit):
         """Update the co-efficients of fitting polynomial
         """
+    
         if self.first_frame_processed:
             left_error = ((self.left_fit[0] - left_fit[0]) ** 2).mean(axis=None)      
             right_error = ((self.right_fit[0] - right_fit[0]) ** 2).mean(axis=None)        
@@ -31,66 +47,36 @@ class Line:
         else:
             self.right_fit = right_fit
             self.left_fit = left_fit
-        
+      
+
         self.update_curvature(self.left_fit)
      
-     
+ 
     def update_curvature(self, fit):
-        """Update radius of curvature
-        
-        y1 = (2*fit[0]*self.y_eval + fit[1])*self.xm_per_pix/self.ym_per_pix
-        y2 = 2*fit[0]*self.xm_per_pix/(self.ym_per_pix**2)
-        curvature = ((1 + y1*y1)**(1.5))/np.absolute(y2)
-     
+        ploty = np.linspace(0, 719, 720 )
+        fitx = fit[0]*ploty**2 + fit[1]*ploty + fit[2]
+
+        if(len(self.recent_xfitted) > self.n):
+            self.recent_xfitted.pop(0)
+
+        self.recent_xfitted.append(fitx)
+        self.bestx = np.mean(self.recent_xfitted,axis=0)
+        self.best_fit = np.polyfit(ploty, self.bestx, 2)
+        self.current_fit = fit
 
 
-        curvature = ((1 + (2*fit[0]*self.y_eval*self.ym_per_pix + fit[1])**2)**1.5) / np.absolute(2*fit[0])
-        #curvature = ((1 + (2*fit[0]*self.y_eval*self.ym_per_pix + fit[1])**2)**1.5) / np.absolute(2*fit[0])        
-        
-        curvature = ((1 + (2*fit[0]*y_0*ym_per_pix + fit[1])**2)**1.5) / np.absolute(2*fit[0])
-        """
+        line_fit=self.best_fit
 
-        ploty = np.linspace(0, 719, num=720)# to cover same y-range as image
-        quadratic_coeff = 3e-4 # arbitrary quadratic coefficient
-        # For each y position generate random x position within +/-50 pix
-        # of the line base position in each case (x=200 for left, and x=900 for right)
-        leftx = np.array([200 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
-                                      for y in ploty])
-
-        leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
-
-
-        # Fit a second order polynomial to pixel positions in each fake lane line
-        left_fit = np.polyfit(ploty, leftx, 2)
-        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-
-
-
-        # Define y-value where we want radius of curvature
-        # I'll choose the maximum y-value, corresponding to the bottom of the image
+        #bottom of image, ie where the car is 
         y_eval = np.max(ploty)
-  
 
-        # Define conversions in x and y from pixels space to meters
-        ym_per_pix = 30/720 # meters per pixel in y dimension
-        xm_per_pix = 3.7/880 # meters per pixel in x dimension
+        plotx = line_fit[0]*ploty**2 + line_fit[1]*ploty + line_fit[2]
 
-        # Fit new polynomials to x,y in world space
-        left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
-        # Calculate the new radii of curvature
-        curvature = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
-
-
-
-
-
-
-
-        if self.first_frame_processed:
-            self.curvature = curvature
+        fit_world = np.polyfit(ploty*self.ym_per_pix, plotx*self.xm_per_pix, 2)
         
-        elif np.absolute(self.curvature - curvature) < 500:
-            self.curvature = 0.75*self.curvature + 0.25*(((1 + y1*y1)**(1.5))/np.absolute(y2)) 
+        curvature = ((1 + (2*fit_world[0]*y_eval*self.ym_per_pix + fit_world[1])**2)**1.5) / np.absolute(2*fit_world[0])
+        self.curvature = curvature
+        #print("Curvature: ",curvature)
 
     def get_position_from_center(self):
         x_left_pix = self.left_fit[0]*(self.y_eval**2) + self.left_fit[1]*self.y_eval + self.left_fit[2]
